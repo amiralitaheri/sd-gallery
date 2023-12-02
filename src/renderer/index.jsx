14feae60ae8c1a8f97b63c7e3e672f461d9f65eb
Amirali Taheri <amiralitaheri64@gmail.com>
2023-12-02 19:25:16 +0000

    build: Install prettier

--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -26,11 +26,11 @@
  * ```
  */
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from "solid-js/web";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-render(() => <App />, root)
+render(() => <App />, root);
--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 // https://vitejs.dev/config
-export default defineConfig({plugins: [solid()],});
+export default defineConfig({ plugins: [solid()] });
