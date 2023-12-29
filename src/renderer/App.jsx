4fb7085ee338112a658c4c02b582a4c816a44386
Amirali Taheri <amiralitaheri64@gmail.com>
2023-12-29 19:58:09 +0000

    feat: Add toast for importing and syncing

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,8 +1,15 @@
 import Dashboard from "./pages/Dashboard";
+import { Toaster } from "solid-toast";
 
 function App() {
   return (
     <>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          duration: 5000,
+        }}
+      />
       <Dashboard />
     </>
   );
--- src/renderer/components/Header.jsx
+++ src/renderer/components/Header.jsx
@@ -4,26 +4,48 @@ import { toggleShowDetails } from "../states/showDetailsSignal";
 import { updateFoldersTree } from "../states/foldersStore";
 import { AddIcon, LayoutSidebarIcon, RestartIcon } from "./icons";
 import { updateImages } from "../states/imagesStore";
+import { toast } from "solid-toast";
+
+const importDirectory = () => {
+  toast.promise(
+    (async () => {
+      const result = await window.sdGalleryApi.importDirectory();
+      await updateFoldersTree();
+      return result;
+    })(),
+    {
+      loading: "Importing new directory.",
+      success: (count) => `${count} images imported successfully.`,
+      error: "An error occurred 😔",
+    },
+  );
+};
+
+const syncDirectory = () => {
+  toast.promise(
+    (async () => {
+      const result = await window.sdGalleryApi.syncDirectories();
+      await updateFoldersTree();
+      await updateImages();
+      return result;
+    })(),
+    {
+      loading: "Syncing directories.",
+      success: ({ added, deleted }) =>
+        `${added} images was added.\n ${deleted} images was deleted.`,
+      error: "An error occurred 😔",
+    },
+  );
+};
 
 const Header = (props) => {
   return (
     <div class={cn(props.class, styles.container)}>
       <div>
-        <button
-          onClick={async () => {
-            await window.sdGalleryApi.importDirectory();
-            await updateFoldersTree();
-          }}
-        >
+        <button onClick={importDirectory}>
           <AddIcon /> Import
         </button>
-        <button
-          onClick={async () => {
-            await window.sdGalleryApi.syncDirectories();
-            await updateFoldersTree();
-            await updateImages();
-          }}
-        >
+        <button onClick={syncDirectory}>
           <RestartIcon />
           Sync
         </button>
