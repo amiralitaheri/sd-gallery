1cd6d07904aa427118a6a1ac9853dfd23e9ff380
Amirali Taheri <amiralitaheri64@gmail.com>
2023-12-30 13:27:59 +0000

    feat: Create actions

--- src/renderer/components/Header.jsx
+++ src/renderer/components/Header.jsx
@@ -1,42 +1,8 @@
 import styles from "./Header.module.pcss";
 import { cn } from "../utils";
 import { toggleShowDetails } from "../states/showDetailsSignal";
-import { updateFoldersTree } from "../states/foldersStore";
 import { AddIcon, LayoutSidebarIcon, RestartIcon } from "./icons";
-import { updateImages } from "../states/imagesStore";
-import { toast } from "solid-toast";
-
-const importDirectory = () => {
-  toast.promise(
-    (async () => {
-      const result = await window.sdGalleryApi.importDirectory();
-      await updateFoldersTree();
-      return result;
-    })(),
-    {
-      loading: "Importing new directory.",
-      success: (count) => `${count} images imported successfully.`,
-      error: "An error occurred 😔",
-    },
-  );
-};
-
-const syncDirectory = () => {
-  toast.promise(
-    (async () => {
-      const result = await window.sdGalleryApi.syncDirectories();
-      await updateFoldersTree();
-      await updateImages();
-      return result;
-    })(),
-    {
-      loading: "Syncing directories.",
-      success: ({ added, deleted }) =>
-        `${added} images was added.\n ${deleted} images was deleted.`,
-      error: "An error occurred 😔",
-    },
-  );
-};
+import { importDirectory, syncDirectory } from "../states/actions";
 
 const Header = (props) => {
   return (
---
+++ src/renderer/states/actions.js
@@ -0,0 +1,40 @@
+import { updateFoldersTree } from "./foldersStore";
+import { updateImages } from "./imagesStore";
+import { toast } from "solid-toast";
+import { updateModels } from "./modelsStore";
+
+// Should the toast message handling be in here or where the method is used?
+
+export const importDirectory = () => {
+  toast.promise(
+    (async () => {
+      const result = await window.sdGalleryApi.importDirectory();
+      await updateFoldersTree();
+      await updateModels();
+      return result;
+    })(),
+    {
+      loading: "Importing new directory.",
+      success: (count) => `${count} images imported successfully.`,
+      error: "An error occurred 😔",
+    },
+  );
+};
+
+export const syncDirectory = () => {
+  toast.promise(
+    (async () => {
+      const result = await window.sdGalleryApi.syncDirectories();
+      await updateFoldersTree();
+      await updateImages();
+      await updateModels();
+      return result;
+    })(),
+    {
+      loading: "Syncing directories.",
+      success: ({ added, deleted }) =>
+        `${added} images was added.\n ${deleted} images was deleted.`,
+      error: "An error occurred 😔",
+    },
+  );
+};
