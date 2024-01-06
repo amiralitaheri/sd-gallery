bec64be87be59b45495c3fadbc106e74ee21cd4f
Amirali Taheri <amiralitaheri64@gmail.com>
2024-01-06 11:31:48 +0000

    feat: Add all button in folder view

--- src/renderer/components/FoldersTreeView.jsx
+++ src/renderer/components/FoldersTreeView.jsx
@@ -1,9 +1,21 @@
 import { foldersTree } from "../states/foldersStore";
 import TreeView from "./TreeView";
+import { imagesStoreState, setSelectedDirectory } from "../states/imagesStore";
+import styles from "./FoldersTreeView.module.pcss";
+import { cn } from "../utils";
 
 const FoldersTreeView = (props) => {
   return (
     <div class={props.class}>
+      <button
+        class={cn(
+          styles.all,
+          !imagesStoreState.params.directoryPath && styles.active,
+        )}
+        onClick={() => setSelectedDirectory(null)}
+      >
+        All
+      </button>
       <TreeView data={foldersTree} />
     </div>
   );
