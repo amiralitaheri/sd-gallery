36e10b52170731659fe8de23db48f34bcb73840a
Amirali Taheri <amiralitaheri64@gmail.com>
2024-01-08 11:02:25 +0000

    refactor: Remove app menu

--- src/main/main.js
+++ src/main/main.js
@@ -19,6 +19,8 @@ const createWindow = () => {
     },
   });
 
+  mainWindow.removeMenu();
+
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
