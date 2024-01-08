8ec06599af0e11b7e529d691acf03ab385952454
Amirali Taheri <amiralitaheri64@gmail.com>
2024-01-08 11:31:19 +0000

    build: Comment out flatpak for now

--- forge.config.js
+++ forge.config.js
@@ -36,10 +36,10 @@ module.exports = {
       name: "@electron-forge/maker-rpm",
       config: {},
     },
-    {
-      name: "@electron-forge/maker-flatpak",
-      config: {},
-    },
+    // {
+    //   name: "@electron-forge/maker-flatpak",
+    //   config: {},
+    // },
   ],
   plugins: [
     {
